daf1ddc227c7e91735178920043d26231256f0a0
kenseikun <sebastian.dziechciarz@wp.pl>
2021-01-19 16:46:28 +0100

    folders preparation

---
+++ src/Root/Root.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Root = () => {
+  return <>Test</>;
+};
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyle from "./GlobalStyles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { mainTheme } from "./GlobalStyles/theme/mainTheme";
+import Root from "./Root/Root";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      <Root />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
